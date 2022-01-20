507408e925f0261fd3881cc19938f63b3117086e
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-01-20 16:32:10 +0100

    name

---
+++ name-popup/script.js
@@ -0,0 +1,4 @@
+
+var color = prompt("give me a color");
+var body = document.getElementsByTagName("body")[0];
+body.style.backgroundColor= color;
---
+++ shell/test.js
@@ -0,0 +1,8 @@
+WScript.StdOut.Write("Enter your name> ")
+var name = WScript.StdIn.ReadLine()
+WScript.StdOut.Write('Goeiedag ' + name)
+
+var numArgs = WScript.arguments.length;
+for (var i = 0; i < WScript.arguments.length; i++) {
+  WScript.StdOut.Write('Hello ' + WScript.arguments(i) + '\n');
+}
