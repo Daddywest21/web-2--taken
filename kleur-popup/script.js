dc995222022f86b1c3fd57657186c8373aee4dc2
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-01-25 14:37:26 +0100

    pop up

---
+++ kleur-popup/script.js
@@ -0,0 +1,5 @@
+var color = prompt("give me a color"); 
+var bodyElement= document.getElementsByTagName("body")[0];
+var Element= document.getElementsByTagName("h1")[0];
+console.log(bodyElement);
+bodyElement.style.backgroundColor= color;
--- name-popup/script.js
+++
@@ -1,5 +0,0 @@
-
-var color = prompt("give me a color");
-var body = document.getElementsByTagName("body")[0];
-console.log(el);
-body.style.backgroundColor= color;
