32cef61b2a5db9f7e71f298f8520230dcfa7566c
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-01-25 15:02:41 +0100

    Update script.js

--- kleur-popup/script.js
+++ kleur-popup/script.js
@@ -1,5 +1,7 @@
-var color = prompt("give me a color"); 
-var bodyElement= document.getElementsByTagName("body")[0];
-var Element= document.getElementsByTagName("h1")[0];
-console.log(bodyElement);
-bodyElement.style.backgroundColor= color;
+var bodyColor = prompt("geef me kleur voor de body"); // we zetten woord gekregen van de gebruiker in de variabele color (zie beetje als lege doos)
+// we gaan een nieuwe variabele declareren genoemd bodyElement
+// en daarin gaan we het HTML-element steken met de naam body
+var bodyElement = document.getElementsByTagName("body")[0];
+// dit stuk code gaat van het body element, de style opvragen (bodyElement.style)
+// vervolgens gaat dit de backgroundColor van het body element aanpassen
+bodyElement.style.backgroundColor = bodyColor;0
