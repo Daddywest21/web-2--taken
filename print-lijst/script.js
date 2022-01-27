90130cd5db21fe59cb64e75f3728220ee56f76ba
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-01-27 16:41:26 +0100

    Update script.js

---
+++ print-lijst/script.js
@@ -0,0 +1,13 @@
+var Acteur1 = {Name: "Tom Holland" , leeftijd: "25j" , Studio: "Marvel Studios" , film: "Spider Man No Way Home" , Genre: "Action , Aventure , Comedy" }
+
+var Acteur2 = {Name: "Mark Ruffalo" , Leeftijd: "54j" , Studio: "Marvel Studios" , film: "Avengers EndGame" , Genre: "Action , Drama , Romance" }
+
+var Acteur3 = {Name: "Denzel Washington" , Leeftijd: "67j" , Studio: "Universal" , film: "Equalizer" , Genre: "Action , Drama , History "  }
+
+var lijstActeur = [Acteur1, Acteur2, Acteur3];
+
+for (let Acteur of lijstActeur) {
+
+    console.log(Acteur);
+}
+
