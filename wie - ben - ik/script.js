8a0f303c66edf7045028f6ef252156d99bae1924
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-02-01 16:40:37 +0100

    updates

---
+++ wie - ben - ik/script.js
@@ -0,0 +1,10 @@
+'use strict'
+console.log('Josue oefening werkt');
+let J1Element = document.querySelector('.derde-rij'); 
+console.log(J1Element);
+J1Element.style.backgroundColor = "yellow"
+
+let Josue = document.querySelector ('#Josue');
+
+Josue.style.textDecoratie= underline;
+
