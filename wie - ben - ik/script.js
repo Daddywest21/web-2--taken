5dd56649c2ecaba08123950c388a497853b199e0
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-02-03 16:36:13 +0100

    update

--- wie - ben - ik/script.js
+++ wie - ben - ik/script.js
@@ -1,10 +1,20 @@
 'use strict'
 console.log('Josue oefening werkt');
-let J1Element = document.querySelector('.derde-rij'); 
-console.log(J1Element);
-J1Element.style.backgroundColor = "yellow"
 
-let Josue = document.querySelector ('#Josue');
+let h1Element = document.querySelector('.h1'); 
+console.log(h1Element);
+h1Element.style.backgroundColor = "yellow"
 
-Josue.style.textDecoratie= underline;
+let josue = document.querySelector ('#Josue');
+josue.style.textDecoration = "underline";
+
+
+let klasseNaam = 'derderij' ; 
+
+
+
+const collection = document.getElementsByClassName("derde-rij");
+for (let element of collection) {
+  element.style.backgroundColor = "yellow";
+}
 
