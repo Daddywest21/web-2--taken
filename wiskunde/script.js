7ca4689a2895123fe33ce0feffb4bd6d690f54a7
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-02-08 14:10:10 +0100

    stap 1 van wiskunde taak

---
+++ wiskunde/script.js
@@ -0,0 +1,6 @@
+// met alert zal er een Popup komen met een bericht erop 
+// in het bericht zal de som komen van 12 + 13 
+let getal1 = prompt('geef het eerste getal');
+let getal2 = prompt('geef het 2de');
+let som = (getal1*1) + (getal2*1); // de som van de getallen boven
+alert(som);
