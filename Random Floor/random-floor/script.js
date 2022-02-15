428f7ace6165aa7c69673d68aa1efedb52efc79b
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-02-15 14:17:09 +0100

    Update script.js

--- Random Floor/random-floor/script.js
+++ Random Floor/random-floor/script.js
@@ -13,16 +13,16 @@ console.log('random nummer tussen 0 en 1 ' + randomGetal)
 
 // om een willekeurige waarde te verkrijgen tussen 0 en een maximum waarde
 // moet je Math.random() vermenigvuldigen met de maximum waarde
-// dus bv om een getal tussen 0 en 10 te krijgen, doe je Math.random() * 10
-let randomGetal10 = Math.random() * 10;
-console.log('random nummer tussen 0 en 10 ' +  randomGetal10)
+// dus bv om een getal tussen 0 en 200 te krijgen, doe je Math.random() * 200
+let randomGetal10 = Math.random() * 200;
+console.log('random nummer tussen 0 en 200 ' +  randomGetal200)
 
-// hier gaan we het random getal tussen 0 en 10 afronden
-let randomGetal10Afgerond = Math.floor(randomGetal10);
-console.log('geheel getal afgerond ' + randomGetal10Afgerond)
+// hier gaan we het random getal tussen 0 en 200 afronden
+let randomGetal10Afgerond = Math.floor(randomGetal200);
+console.log('geheel getal afgerond ' + randomGetal200Afgerond)
 
 // OPDRACHT
-// hier zie je hoe je een willekeurig getal uitprint tussen 0 en 10
+// hier zie je hoe je een willekeurig getal uitprint tussen 0 en 200
 // en dan zie je hoe we dat getal afronden (cijfers na de komma weghalen)
 // 1. een willekeurig getal uitprinten die tussen 0 en 255 is > gebruik Math.random()
 // 2. en dan van dat getal, alle cijfers na de komma wegdoen > gebruik Math.floor()
