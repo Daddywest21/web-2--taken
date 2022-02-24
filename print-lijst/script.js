a08d7ce5872cd2d4f2f3add2e324d78f528bf1c6
Josue dj <94456387+Daddywest21@users.noreply.github.com>
2022-02-24 12:16:23 +0100

    udt

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -11,3 +11,10 @@ for (let Acteur of lijstActeur) {
     console.log(Acteur);
 }
 
+const namen = ["Dirk", "Sandy", "Tormund", "Dahlia"];
+
+for (let i = 0; i < namen.length; i++) {
+    // Gebruik de teller i als index van de array
+    // Elke iteratie wordt er dus een nieuw element aangesproken
+    console.log(namen[i]);
+}
